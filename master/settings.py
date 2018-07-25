b6fc68f3d0b2398a64a5be02f14f2dfc7bb6efec
Ahmed <ahmed.b.yahia@gmail.com>
2018-07-25 22:29:45 +0900

    First test

--- master/settings.py
+++ master/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-ALLOWED_HOSTS = ['tranquil-dawn-64750.herokuapp.com']
+ALLOWED_HOSTS = ['lotfitower.herokuapp.com']
 
 STATIC_URL = '/static/'
 
