2dbe1153341adba9251ac2b7fedcf76caf9351ab
Ahmed <ahmed.b.yahia@gmail.com>
2018-07-28 21:14:12 +0900

    Update2

--- master/settings.py
+++ master/settings.py
@@ -133,8 +133,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static'),
 ]
-MEDIA_URL = "/media/"
-STATIC_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 
 try:
